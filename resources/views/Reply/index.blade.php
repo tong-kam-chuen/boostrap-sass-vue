4d26bf20a3fc524f8b606df26ce05cbbe4a5ff32
Ray <feraltech.ray@gmail.com>
2020-05-21 10:27:50 +0800

    Commit signature pad

--- resources/views/Reply/index.blade.php
+++ resources/views/Reply/index.blade.php
@@ -127,10 +127,30 @@
                         <?php
                             case 'canvas':
                          ?>
+                        <style>
+                        #signature{
+                         width: 300px; height: 200px;
+                         border: 1px solid black;
+                        }
+                        </style>
+
+                        <!-- Signature -->
+                        <div id="signature" style=''>
+                         <canvas id="signature-pad" class="signature-pad" width="300px" height="200px"></canvas>
+                        </div><br/>
+
+                        <input type='button' id='click' value='preview'><br/>
+                        <textarea id='output'></textarea><br/>
+
+                        <!-- Preview image -->
+                        <img src='' id='sign_prev' style='display: none;' />
+
                         <center>
                             <canvas id="image" style="max-width:100%; border:1px solid #000000;">
                             </canvas>
+                            <canvas id="signature-pad" class="signature-pad" width="300px" height="200px"></canvas>
                         </center>
+
                         <a href="#" id="resetbtn" class="btn btn-success" style="width:125px; height:34px;">
                             Reset
                         </a>
@@ -177,53 +197,85 @@
 
 </div>
 
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+
+<!-- Bootstrap JavaScript -->
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.1/js/bootstrap.min.js"></script>
+
+<!-- toastr notifications -->
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+
+<!-- icheck checkboxes -->
+<script type="text/javascript" src="{{ asset('icheck/icheck.min.js') }}"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+     var signaturePad = new SignaturePad(document.getElementById('signature-pad'));
+
+     $('#click').click(function(){
+      var data = signaturePad.toDataURL('image/png');
+      $('#output').val(data);
+
+      $("#sign_prev").show();
+      $("#sign_prev").attr("src",data);
+      // Open image in the browser
+      //window.open(data);
+     });
+    })
+ </script>
+
 <script>
 
-$(document).on('click', '#save_image', function() {
-    var canvas = document.getElementById('image');
-    var dataURL = canvas.toDataURL();
-    $.ajax({
-      type: "POST",
-      url: "/saveImage",
-      data: {
-          imgBase64: dataURL
-      }
-    }).done(function(data) {
-        console.log('Image saved');
+    $(document).on('click', '#save_image', function() {
+        var signaturePad = new SignaturePad(document.getElementById('signature-pad'));
+        var canvas = document.getElementById('signature-pad');
+        var dataURL = canvas.toDataURL();
+        $.ajax({
+          type: "POST",
+          url: "/saveImage",
+          data: {
+              imgBase64: dataURL
+          }
+        }).done(function(data) {
+            console.log('Image saved');
 
-        // Do here whatever you want.
-    });
-});
-
-$(function() {
-    var canvas = document.getElementById('image');
-    var ctx = canvas.getContext('2d');
-    var img = new Image();
-    img.crossOrigin = '';
-    img.src = '{{ url(dirname(public_path()) . '/' . basename(public_path())) }}';
-    img.onload = function() {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0, img.width, img.height);
-    }
-    var $reset = $('#resetbtn');
-    var $noise = $('#noisebtn');
-    $reset.on('click', function(e) {
-        e.preventDefault();
-        $('input[type=range]').val(0);
-        Caman('#image', img, function() {
-            this.revert(false);
-            this.render();
+            // Do here whatever you want.
         });
     });
-    $noise.on('click', function(e) {
-        e.preventDefault();
-        Caman('#image', img, function() {
-                this.noise(10).render();
-            });
-    });
 
-});
+    $(function() {
+        var signaturePad = new SignaturePad(document.getElementById('signature-pad'));
+        var canvas = document.getElementById('signature-pad');
+        var ctx = canvas.getContext('2d');
+        var img = new Image();
+        img.crossOrigin = '';
+        img.src = '{{ url(dirname(public_path()) . '/' . basename(public_path())) }}';
+        img.onload = function() {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0, img.width, img.height);
+        }
+        var $reset = $('#resetbtn');
+        var $noise = $('#noisebtn');
+        $reset.on('click', function(e) {
+            e.preventDefault();
+            $('input[type=range]').val(0);
+            Caman('#image', img, function() {
+                this.revert(false);
+                this.render();
+            });
+        });
+        $noise.on('click', function(e) {
+            e.preventDefault();
+            Caman('#image', img, function() {
+                    this.noise(10).render();
+                });
+        });
+
+    });
 
 </script>
 
