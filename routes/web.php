f4bcde02b849a64766c39b2bf5472e492d097e37
Ray <feraltech.ray@gmail.com>
2020-05-27 13:57:54 +0800

    Commit Home Route Fallback

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,20 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::resource('Replies','ReplyController');
-Route::resource('Options','OptionController');
-Route::resource('Questions','QuestionController');
-Route::resource('Questionnaires','QuestionnaireController');
-Route::post('Questionnaires/changeStatus', array('as' => 'changeStatus', 'uses' => 'QuestionnaireController@changeStatus'));
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth']], function() {
+
+  Route::get('/', function () {
+      return redirect('dashboard');
+  });
+
+  Route::resource('Replies','ReplyController');
+  Route::resource('Options','OptionController');
+  Route::resource('Questions','QuestionController');
+  Route::resource('Questionnaires','QuestionnaireController');
+  Route::post('Questionnaires/changeStatus', array('as' => 'changeStatus', 'uses' => 'QuestionnaireController@changeStatus'));
+
+});
+
+Route::get('{path}', "HomeController@index")->where('path', '([A-z\d/-\/_.]+)?');
